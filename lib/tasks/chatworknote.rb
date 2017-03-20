2eff1077077a9a98036087508690dbb93ff75d4d
Kana <kana_macbookair@Kana-no-MacBook-Air.local>
2017-03-20 20:14:13 +0900

    chatworknote.rbの修正と追加

---
+++ lib/tasks/chatworknote.rb
@@ -0,0 +1,38 @@
+require "#{Rails.root}/app/models/user"
+require "#{Rails.root}/app/models/history"
+require "chatwork"
+
+module Tasks
+end
+
+class Tasks::Chatworknote
+  def self.decide_message
+    s=Date.today.wday
+    case s
+    when 1,4
+      "燃えるごみを捨ててください"
+    when 2
+      "資源ごみを捨ててください"
+    else
+      h=History.last
+      h.user.name
+      #{}"何も捨てなくていいです"
+    end
+  end
+
+  def self.decide_toids
+    h=History.last
+    h.user.chatworkid
+  end
+
+  def self.notify
+    message=decide_message
+    toids=decide_toids
+    ChatWork.api_key =""
+    ChatWork::Task.create(room_id: "",body: message,to_ids:toids)
+  end
+
+end
+
+#builder=Chatworknote.new
+#builder.notify
